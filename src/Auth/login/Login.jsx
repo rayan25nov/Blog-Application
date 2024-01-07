87559602298cb3158f068df9c277964f7946101b
Rayan Ahmad <rayan25nov@gmail.com>
2024-01-07 10:13:43 +0530

    Changed signin letter

--- src/Auth/login/Login.jsx
+++ src/Auth/login/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
             />
             {error && <div className={styles.error_msg}>{error}</div>}
             <button type="submit" className={styles.green_btn}>
-              Sing In
+              Sign In
             </button>
           </form>
         </div>
@@ -61,7 +61,7 @@ const Login = () => {
           <h1>New Here ?</h1>
           <Link to="/signup">
             <button type="button" className={styles.white_btn}>
-              Sing Up
+              Sign Up
             </button>
           </Link>
         </div>
