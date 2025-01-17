0be91172decf9dd1d0d006ac2b08fd37433944cd
Rayan Ahmad <rayan25nov@gmail.com>
2025-01-18 00:52:56 +0530

    fixed enum error due to gender first character is capital in schema

--- src/Routes/Profile/EditProfile.jsx
+++ src/Routes/Profile/EditProfile.jsx
@@ -265,9 +265,9 @@ const EditProfile = ({ user }) => {
                   <option value="" disabled>
                     Select Gender
                   </option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
                 </select>
               </div>
             </div>
