f3f7efde2cf5f3a7b98e712e979f1d0fb0ed0037
Rayan Ahmad <rayan25nov@gmail.com>
2025-01-18 00:40:31 +0530

    Completed profile update section

--- src/Routes/Profile/EditProfile.jsx
+++ src/Routes/Profile/EditProfile.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Cropper from "react-easy-crop";
 import styles from "./EditProfile.module.css";
 import Email from "../../assets/images/email.png";
 import Camera from "../../assets/images/camera.svg";
+import Eye from "../../assets/images/eye.svg";
+import EyeOff from "../../assets/images/eye-off.svg";
+import axios from "axios";
+import countryList from "react-select-country-list";
+import { toast } from "react-toastify";
 
 const EditProfile = ({ user }) => {
   if (!user) {
@@ -14,6 +19,18 @@ const EditProfile = ({ user }) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 }); // Crop position
   const [zoom, setZoom] = useState(1); // Zoom level
   const [isCropping, setIsCropping] = useState(false); // Toggle crop modal
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [password, setPassword] = useState("");
+
+  const [gender, setGender] = useState(user.gender ? user.gender : "");
+  const [country, setCountry] = useState(user.country ? user.country : "");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const uploadImage = () => {
     const input = document.createElement("input");
@@ -83,6 +100,84 @@ const EditProfile = ({ user }) => {
 
   const date = new Date(Date.now()).toDateString();
   const createdAt = new Date(user.createdAt).toDateString();
+  const options = useMemo(() => countryList().getData(), []);
+  const handleCountryInput = (e) => {
+    const userInput = e.target.value;
+    setCountry(userInput);
+
+    if (userInput.trim() === "") {
+      setFilteredCountries([]);
+      return;
+    }
+
+    // Filter city names based on user input
+    const suggestions = options.filter((name) =>
+      name.label.toLowerCase().startsWith(userInput.toLowerCase())
+    );
+
+    setFilteredCountries(suggestions);
+  };
+
+  const base64ToFile = (base64, filename) => {
+    const arr = base64.split(",");
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  };
+
+  const updateProfileHandler = async () => {
+    console.log(name, email, password, gender, country);
+    const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+    const JWT_TOKEN = localStorage.getItem("token");
+    try {
+      if (
+        password.trim() === "" ||
+        name.trim() === "" ||
+        email.trim() === "" ||
+        country.trim() === "" ||
+        gender.trim() === ""
+      ) {
+        toast.error("All Fields are required");
+        return;
+      }
+      if (password.length < 6) {
+        toast.error("Password must be at least 6 characters");
+        return;
+      }
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("country", country);
+      formData.append("gender", gender);
+
+      // Convert profileImage (base64) to File
+      if (profileImage.startsWith("data:image")) {
+        const file = base64ToFile(profileImage, "profile.jpg");
+        formData.append("image", file);
+      }
+
+      const { data: res } = await axios.put(
+        `${apiUrl}/users/profile`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${JWT_TOKEN}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      toast.error("Error updating profile");
+      console.error("Error while updating user data:", error);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -111,28 +206,97 @@ const EditProfile = ({ user }) => {
               <h2>{user.name}</h2>
               <p>{user.email}</p>
             </div>
-            <button className={styles.editButton}>Edit</button>
+            <button
+              className={styles.editButton}
+              onClick={updateProfileHandler}
+            >
+              Edit
+            </button>
           </div>
           <div className={styles.profileDetails}>
             <div className={styles.formGroup}>
               <label>Full Name</label>
-              <input type="text" placeholder="Your First Name" />
+              <input
+                type="text"
+                placeholder="Your First Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)} // Fix here
+                required={true}
+              />
             </div>
             <div className={styles.formGroup}>
               <label>Email</label>
-              <input type="text" placeholder="Your Email" />
+              <input
+                type="text"
+                placeholder="Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} // Fix here
+                required={true}
+              />
             </div>
+
+            <div className={styles.formGroup}>
+              <label>Password</label>
+              <div className={styles.passwordInput}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required={true}
+                />
+                <img
+                  src={showPassword ? EyeOff : Eye}
+                  alt="toggle password visibility"
+                  onClick={togglePasswordVisibility}
+                  className={styles.togglePassword}
+                />
+              </div>
+            </div>
+
             <div className={styles.formGroup}>
               <label>Gender</label>
-              <input type="text" placeholder="Your Gender" />
+              <div className={styles.customSelect}>
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)} // Update gender state
+                  required={true}
+                >
+                  <option value="" disabled>
+                    Select Gender
+                  </option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
             </div>
             <div className={styles.formGroup}>
               <label>Country</label>
-              <input type="text" placeholder="Country" />
-            </div>
-            <div className={styles.formGroup}>
-              <label>Language</label>
-              <input type="text" placeholder="Language" />
+              <input
+                type="text"
+                placeholder="Country"
+                value={country}
+                onChange={handleCountryInput}
+                required={true}
+              />
+              {filteredCountries.length > 0 && (
+                <ul className={styles.suggestionsList}>
+                  {filteredCountries.map((name, index) => (
+                    <li
+                      key={index}
+                      className={styles.suggestionItem}
+                      // Update country state on click also reset suggestions
+                      onClick={() => {
+                        setCountry(name.label);
+                        setFilteredCountries([]);
+                      }}
+                    >
+                      {name.label}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           </div>
           <div className={styles.emailSection}>
--- src/Routes/Profile/Profile.jsx
+++ src/Routes/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import Card from "../Card/Card";
 import { useSelector } from "react-redux";
 import { selectDarkMode } from "../../Features/ToggleModeSlice";
 import styles from "./Profile.module.css";
+import { Link } from "react-router-dom";
 
 const Profile = (props) => {
   const darkMode = useSelector(selectDarkMode);
@@ -15,17 +16,20 @@ const Profile = (props) => {
       className={`${styles.profile} ${darkMode ? styles.dark : styles.light}`}
     >
       <div className={styles.user_description}>
+        <img
+          src={props.user.image}
+          alt={props.user.name}
+          className={styles.profile_image}
+        />
         <h1 className={styles.profile_name}>{props.user.name}</h1>
         <p className={styles.profile_email}>Email: {props.user.email}</p>
         <p className={styles.profile_role}>Role: {props.user.role}</p>
         <p className={styles.profile_verified}>
           {props.user.verified ? "Verified" : "Not verified"}
         </p>
-        <img
-          src={props.user.image}
-          alt={props.user.name}
-          className={styles.profile_image}
-        />
+        <Link to={"/profile-edit"} className={styles.btn}>
+          Update Profile
+        </Link>
       </div>
       <div className={styles.profile_posts}>
         <h2 className={styles.profile_posts_title}>Posts</h2>
